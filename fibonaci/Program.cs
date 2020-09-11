64bd343742ce4229e347123110f7aa46211768d7
sevketasurgan <43352546+sevketasurgan@users.noreply.github.com>
2020-09-11 14:54:42 +0300

    Add files via upload

---
+++ fibonaci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+namespace fibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Fibonacci Kaça kadar gitsin : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            System.Console.Write("\n");
+                FibonacciHesapla(input);
+                
+        }
+      
+       static void  FibonacciHesapla(int limit)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            for (int i = 1; i < limit; i++)
+            {
+                decimal toplam = Convert.ToInt32(arrayList[i]) + Convert.ToInt32(arrayList[i-1]);
+                arrayList.Add(toplam);
+            }
+            foreach (var item in arrayList) System.Console.WriteLine($"{arrayList.IndexOf(item) + 1} => {item}");
+     
+        }
+    }
+}
